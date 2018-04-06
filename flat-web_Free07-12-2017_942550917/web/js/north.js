8637b7d29954a3d3d83e6b73ba01ef74af7cc3e8
Abhishek <sil.abhishek97@gmail.com>
2018-04-07 00:26:41 +0530

    north.js and south.js
    updated

---
+++ flat-web_Free07-12-2017_942550917/web/js/north.js
@@ -0,0 +1,234 @@
+export const northListing = [
+    {   name:BMPG 1
+        sex: "Boys",
+        newPlace: true,
+        place: "Kamla Nagar(Roop Nagar)",
+        nearbyCollege: "SRCC, Kirori Mal College, Ramjas College, Hindu College, SGTB Khalsa, St. Stephen's College and Hansraj College",
+		nearbyMetro: "GTB Nagar and Vishwavidyalaya",
+        sharing: "Single, Double and Triple sharing rooms are available",
+        facilities: [
+            "Fully furnished", 
+"4 times delicious meals",
+"Garden Area",
+"R.O purified Water",
+"Bathroom equipped with geysers",
+"Fully functional kitchen",
+"24*7 security and functional CCTV surveillance",
+"Hi speed wifi",
+"Laundry Facility", 
+"24 hours running coffee and tea machine", 
+"Lemonade and Ice tea facility", 
+"Free of cost transportation facility"
+"Personal Televisions with Tata Sky HD in every Room", 
+"Gym Facility", 
+ "PS4, Netflix, Snooker Table, Table Tennis, Air Hockey"                 
+        ],
+        
+        images: [],
+        thumbnail: "https://img.staticmb.com/mbphoto/property/cropped_images/2016/Aug/25/Photo_h180_w240/21922755_2_DSC_0038_180_240.jpg",
+    },
+    {   name:BMPG 2
+        sex: "Girls",
+        newPlace: true,
+        place: "Kamla Nagar(Roop Nagar)"
+nearbyCollege: "SRCC, Kirori Mal College, Ramjas College, Hindu College, SGTB Khalsa, St. Stephen's College and Hansraj College",
+		nearbyMetro: "GTB Nagar and Vishwavidyalaya",
+        sharing: "Single, Double and Tripple sharing rooms are available",
+        facilities: [
+            "Fully furnished", 
+"4 times delicious meals",
+"Small garden",
+"R.O purified Water",
+"Bathroom equipped with geysers",
+"Fully functional kitchen",
+"24*7 security and functional CCTV surveillance",
+"Hi speed wifi",  "24 hours running coffee and tea machine", "Lemonade and Ice tea facility",
+"Laundry Facility", "Personal Televisions with Tata Sky HD in every Room","Free of cost transportation facility"
+        ],
+       
+        images: [],
+        thumbnail: "https://lh6.googleusercontent.com/-SUFzj-Z7_P4/AAAAAAAAAAI/AAAAAAAAABQ/F6WRgAztFDA/photo.jpg",
+    },
+    
+    {   name: BMPG 3
+        sex: "Boys",
+        newPlace: true,
+        place: "Shakti nagar",
+        nearbyCollege: " SRCC, Kirori Mal College, Ramjas College, Hindu College, SGTB Khalsa, St. Stephen's College and Hansraj College.",
+        nearbyMetro:"GTB Nagar, Vishwavidyalaya, Azadpur and Shastri Nagar",
+		sharing: "Single, Double and Tripple sharing rooms are available",
+        facilities: [
+            "Fully furnished", 
+"4 times delicious meals",
+"R.O purified Water",
+"Bathroom equipped with geysers",
+"Fully functional kitchen",
+"24*7 security and functional CCTV surveillance",
+"Hi speed wifi",
+"Laundry Facility",  "24 hours running coffee and tea machine", "Lemonade and Ice tea facility",
+"Pool table and table tennis."
+        ],
+       
+        images: [],
+        thumbnail: "https://img.staticmb.com/mbphoto/property/cropped_images/2016/Aug/25/Photo_h180_w240/21922755_2_DSC_0038_180_240.jpg",
+    },
+	   
+	   {   name: BMPG 4
+        sex: "Girls",
+        newPlace: false,
+        place: "Laxmi nagar",
+        nearbyCollege: " Institute for CA Aspirants",
+        nearbyMetro:"Laxmi Nagar",
+		sharing: "Double sharing rooms are available",
+,
+        facilities: [
+           "Fully furnished", 
+"3 times delicious meals",
+"R.O purified Water",
+"Bathroom equipped with geysers",
+"Fully functional kitchen",
+"Hi speed wifi",
+"Laundry Facility",
+"Terrace area and 2 wheeler parking",
+"Gated-society with full security"
+        ],
+       
+        images: [],
+        thumbnail: "https://img.staticmb.com/mbphoto/property/cropped_images/2016/Aug/25/Photo_h180_w240/21922755_2_DSC_0038_180_240.jpg",
+    },
+    {   name: BMPG 5
+        sex: "Boys",
+        newPlace: true,
+        place: "Shakti nagar",
+        nearbyCollege: "SRCC, Kirori Mal College, Ramjas College, Hindu College, SGTB Khalsa, St. Stephen's College and Hansraj College",
+        nearbyMetro: "GTB Nagar, Vishwavidyalaya, Azadpur and Shastri Nagar",
+		sharing: "Single, Double and Tripple sharing rooms are available",
+        facilities: [
+           "Fully furnished", 
+"4 times delicious meals",
+"Garden Area",
+"R.O purified Water",
+"Bathroom equipped with geysers",
+"Fully functional kitchen",
+"24*7 security and functional CCTV surveillance",
+"Hi speed wifi",
+"Laundry Facility",  "24 hours running coffee and tea machine", "Lemonade and Ice tea facility"
+"Personal Televisions with Tata Sky HD in every Room", "Gym Facility", "PS4, Netflix, Snooker Table, Table Tennis, Air Hockey",
+"Swimming pool",
+"Free of cost transportation facility"
+        ],
+        
+        images: [],
+        thumbnail: "https://img.staticmb.com/mbphoto/property/cropped_images/2016/Aug/25/Photo_h180_w240/21922755_2_DSC_0038_180_240.jpg",
+    },
+    {   name: BMPG 6
+        sex: "Girls",
+        newPlace: true,
+        place: "Vijay Nagar",
+        nearbyCollege: "SRCC, Kirori Mal College, Ramjas College, Hindu College, SGTB Khalsa, St. Stephen's College and Miranda House",
+        nearbyMetro: "GTB Nagar and Vishwavidyalaya",
+		sharing: "Double and Tripple sharing rooms are available",
+        facilities: [
+            "Fully furnished", 
+"4 times delicious meals",
+"R.O purified Water",
+"Bathroom equipped with geysers",
+"Fully functional kitchen",
+"24*7 security and functional cctv surveillance",
+"Hi speed wifi",
+"Laundry Facility",  "24 hours running coffee and tea machine", "Lemonade and Ice tea facility",
+"Gym at discounted price". "Personal Televisions with Tata Sky HD in every Room",
+"Free of cost transportation facility"
+        ],
+        
+        images: [],
+        thumbnail: "https://img.staticmb.com/mbphoto/property/cropped_images/2016/Aug/25/Photo_h180_w240/21922755_2_DSC_0038_180_240.jpg",
+    },
+    {   name: BMPG 7
+        sex: "Girls",
+        newPlace: true,
+        place: "Vijay Nagar",
+        nearbyCollege: "SRCC, Kirori Mal College, Ramjas College, Hindu College, SGTB Khalsa, St. Stephen's College and Miranda House",
+        nearbyMetro: "GTB Nagar and Vishwavidyalaya",
+        sharing: "Double and Tripple sharing rooms are available",
+        facilities: [
+           "Fully furnished", 
+"4 times delicious meals",
+"R.O purified Water",
+"Bathroom equipped with geysers",
+"Fully functional kitchen",
+"24*7 security and functional CCTV surveillance",
+"Hi speed wifi",
+"Library"
+        ],
+       
+        images: [],
+        thumbnail: "https://img.staticmb.com/mbphoto/property/cropped_images/2016/Aug/25/Photo_h180_w240/21922755_2_DSC_0038_180_240.jpg",
+    },
+	{    name: BMPG 8
+        sex: "Girls",
+        newPlace: true,
+        place: "Hudson Lane",
+        nearbyCollege: "SRCC, Kirori Mal College, Ramjas College, Hindu College, SGTB Khalsa, St. Stephen's College and Miranda House",
+		nearbyMetro:" GTB Nagar and Vishwavidyalaya.",
+        sharing: "Double and Tripple sharing rooms are available.",
+        facilities: [
+            "Fully furnished", 
+"4 times delicious meals",
+"R.O purified Water",
+"Bathroom equipped with geysers",
+"Fully functional kitchen",
+"24*7 security and functional cctv surveillance",
+"Hi speed wifi",
+"Laundry Facility",
+"Library"
+        ],
+       
+        images: [],
+        thumbnail: "https://lh6.googleusercontent.com/-SUFzj-Z7_P4/AAAAAAAAAAI/AAAAAAAAABQ/F6WRgAztFDA/photo.jpg",
+    },
+    {   name: BMPG 9
+        sex: "Girls",
+        newPlace: true,
+        place: "Civil lines",
+        nearbyCollege:"IP College for Women, NBCC and Ambedkar College",
+        nearbyMetro: "Civil Lines and Kashmere Gate",
+		sharing: "Double and Tripple sharing rooms are available.",
+        facilities: [
+           "Fully furnished", 
+"4 times delicious meals",
+"R.O purified Water",
+"Bathroom equipped with geysers",
+"Fully functional kitchen",
+"24*7 security and functional cctv surveillance",
+"Hi speed wifi",
+"Laundry Facility",
+"Small Reading Room",
+"Snooker table and Pool table","Free of cost transportation facility"
+        ],
+       
+        images: [],
+        thumbnail: "https://lh6.googleusercontent.com/-SUFzj-Z7_P4/AAAAAAAAAAI/AAAAAAAAABQ/F6WRgAztFDA/photo.jpg",
+   },
+   
+	{    name: BMPG 10
+        sex: "Boys",
+        newPlace: true,
+        place: "Laxmi Nagar",
+        nearbyCollege: "Institute for CA Aspirants",
+		nearbyMetro:"Laxmi Nagar",
+        sharing: "Double and Triple  sharing rooms available.",
+        facilities: [
+           "Fully furnished", 
+"3 times delicious meals",
+"R.O purified Water",
+"Bathroom equipped with geysers",
+"Fully functional kitchen",
+"24*7 security and functional CCTV surveillance",
+"Hi speed wifi"
+        ],
+       
+        images: [],
+        thumbnail: "https://lh6.googleusercontent.com/-SUFzj-Z7_P4/AAAAAAAAAAI/AAAAAAAAABQ/F6WRgAztFDA/photo.jpg",
+    },
+];
---
+++ flat-web_Free07-12-2017_942550917/web/js/south.js
@@ -0,0 +1,150 @@
+export const southListing = [
+    {   name:BMPG 11
+        sex: "Girls",
+        newPlace: true,
+        place: "Greater Kailash-1, Block- B",
+        nearbyCollege: " LSR College, NIFT, Gargi College and Kamla Nehru College,Pratham Institute",
+		nearbyMetro: "Moolchand and Greater Kailash",
+        sharing: "Single, Double and Triple sharing rooms are available",
+        faci"Fully furnished", 
+"4 times delicious meals",
+"R.O purified Water",
+"Bathroom equipped with geysers",
+"Fully functional kitchen",
+"24*7 security and functional cctv surveillance",
+"Hi speed wifi",
+"Laundry Facility",
+"Small Reading Room",
+"Snooker table and Pool table", "Free of cost transportation facility"              
+        ],
+        
+        images: [],
+        thumbnail: "https://img.staticmb.com/mbphoto/property/cropped_images/2016/Aug/25/Photo_h180_w240/21922755_2_DSC_0038_180_240.jpg",
+    },
+    {   name:BMPG 12
+        sex: "Girls",
+        newPlace: true,
+        place: "SDA(South Delhi)"
+nearbyCollege: "NIFT, Venkateswara College, Jesus and Mary College, Motilal College,  Gargi College, Kamla Nehru College and AIIMS. ",
+		nearbyMetro: "Green Park, AIIMS and Hauz Khaz",
+        sharing: "Double and Tripple sharing rooms available",
+        facilities: [
+            "Fully furnished", 
+"4 times delicious meals",
+"Garden Area",
+"R.O purified Water",
+"Bathroom equipped with geysers",
+"Fully functional kitchen",
+"24*7 security and functional CCTV surveillance",
+"Hi speed wifi",
+"Laundry Facility", "24 hours running coffee and tea machine", "Lemonade and Ice tea facility", "Free of cost transportation facility"
+"Personal Televisions with Tata Sky HD in every Room", "Gym Facility",  "PS4, Netflix, Snooker Table, Table Tennis, and Air Hockey", " Seperate Cafeteria Area".               
+        ],
+       
+        images: [],
+        thumbnail: "https://lh6.googleusercontent.com/-SUFzj-Z7_P4/AAAAAAAAAAI/AAAAAAAAABQ/F6WRgAztFDA/photo.jpg",
+    },
+    
+    {   name: BMPG 13
+        sex: "Girls",
+        newPlace: true,
+        place: "Anand Niketan(South Campus)",
+        nearbyCollege: "  Venkateswara College, Jesus and Mary College, Motilal College, Gargi College, Kamla Nehru College, Aryabhatt College and Maitreyi College",
+        nearbyMetro:"Durgabai Deshmukh South Campus",
+		sharing: "Double, Triple, Four and Six sharing rooms available",
+        facilities: [
+           "Fully furnished", 
+"3 times delicious meals",
+"R.O purified Water",
+"Bathroom equipped with geysers",
+"Fully functional kitchen",
+"24*7 security and functional CCTV surveillance",
+"Hi speed wifi"
+        ],
+       
+        images: [],
+        thumbnail: "https://img.staticmb.com/mbphoto/property/cropped_images/2016/Aug/25/Photo_h180_w240/21922755_2_DSC_0038_180_240.jpg",
+    },
+	   
+	   {   name: BMPG 14
+        sex: "Girls",
+        newPlace: false,
+        place: "Greater Kailash-1, Block N",
+        nearbyCollege: " LSR College, NIFT, Gargi College and Kamla Nehru College,Pratham Institute",
+		nearbyMetro: "Moolchand and Greater Kailash",
+		sharing: "Double, Triple, Four and Six sharing rooms available",
+,
+        facilities: [
+          "Fully furnished", 
+"3 times delicious meals",
+"R.O purified Water",
+"Bathroom equipped with geysers",
+"Fully functional kitchen",
+"24*7 security and functional CCTV surveillance",
+"Hi speed wifi", "Garden Area", "24 hours running coffee and tea machine"
+        ],
+       
+        images: [],
+        thumbnail: "https://img.staticmb.com/mbphoto/property/cropped_images/2016/Aug/25/Photo_h180_w240/21922755_2_DSC_0038_180_240.jpg",
+    },
+    {   name: BMPG 15
+        sex: "Girls",
+        newPlace: true,
+        place: "Anand Niketan(South Campus)",
+        nearbyCollege: "  Venkateswara College, Jesus and Mary College, Motilal College, Gargi College, Kamla Nehru College, Aryabhatt College and Maitreyi College",
+        nearbyMetro:"Durgabai Deshmukh South Campus",
+		sharing: "Double, Triple, Four and Six sharing rooms available",
+        facilities: [
+           "Fully furnished", 
+"3 times delicious meals",
+"R.O purified Water",
+"Bathroom equipped with geysers",
+"Fully functional kitchen",
+"24*7 security and functional CCTV surveillance",
+"Hi speed wifi"
+        ],
+        
+        images: [],
+        thumbnail: "https://img.staticmb.com/mbphoto/property/cropped_images/2016/Aug/25/Photo_h180_w240/21922755_2_DSC_0038_180_240.jpg",
+    },
+    {   name: BMPG 16
+        sex: "Girls",
+        newPlace: true,
+        place: "Greater Kailash-1, Block C",
+        nearbyCollege: " LSR College, NIFT, Gargi College and Kamla Nehru College,Pratham Institute",
+		nearbyMetro: "Moolchand and Greater Kailash",
+		sharing: "Double and Triple  sharing rooms available",
+        facilities: [
+            "Fully furnished", 
+"4 times delicious meals",
+"R.O purified Water",
+"Bathroom equipped with geysers",
+"Fully functional kitchen",
+"24*7 security and functional CCTV surveillance",
+"Hi speed wifi", "In-house Gym"
+        ],
+        
+        images: [],
+        thumbnail: "https://img.staticmb.com/mbphoto/property/cropped_images/2016/Aug/25/Photo_h180_w240/21922755_2_DSC_0038_180_240.jpg",
+    },
+    {   name: BMPG 17
+        sex: "Girls",
+        newPlace: true,
+         place: "Anand Niketan(South Campus)",
+        nearbyCollege: "  Venkateswara College, Jesus and Mary College, Motilal College, Gargi College, Kamla Nehru College, Aryabhatt College and Maitreyi College",
+        nearbyMetro:"Durgabai Deshmukh South Campus",
+        sharing: "Double, Triple and Four  sharing rooms available",
+        facilities: [
+          "Fully furnished", 
+"3 times delicious meals",
+"R.O purified Water",
+"Bathroom equipped with geysers",
+"Fully functional kitchen",
+"24*7 security and functional CCTV surveillance",
+"Hi speed wifi"
+        ],
+       
+        images: [],
+        thumbnail: "https://img.staticmb.com/mbphoto/property/cropped_images/2016/Aug/25/Photo_h180_w240/21922755_2_DSC_0038_180_240.jpg",
+    },
+	];
